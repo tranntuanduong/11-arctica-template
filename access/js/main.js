0069e7236099bc4a5dffb16ec70395e968873585
tranntuanduong <tranntuanduong@gmail.com>
2021-03-08 11:55:43 +0700

    title-session Please enter the commit message for your changes. Lines starting

--- access/js/main.js
+++ access/js/main.js
@@ -5,12 +5,13 @@ $(function(){
     })
 
 
-   $('.menus-filter-navigation-btn').click(function() {
+    $('.menus-filter-navigation-btn').click(function() {
         $('.menus-filter-navigation-btn').removeClass('menus-filter-navigation-btn--active')
         $(this).addClass('menus-filter-navigation-btn--active')
         data = $(this).data('filter'); 
         filterItems.isotope({ filter:  data })
-        return false ; 
-   });
+        return false ;      
+    });
 
+   
 })  
