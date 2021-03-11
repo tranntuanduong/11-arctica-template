6ac05a32b0b56ca71f121fb2cacd9bb90adb2052
tranntuanduong <tranntuanduong@gmail.com>
2021-03-11 16:51:22 +0700

    update: shop detail page

--- access/js/main.js
+++ access/js/main.js
@@ -4,7 +4,6 @@ $(function(){
         layoutMode: 'fitRows'
     })
 
-
     $('.menus-filter-navigation-btn').click(function() {
         $('.menus-filter-navigation-btn').removeClass('menus-filter-navigation-btn--active')
         $(this).addClass('menus-filter-navigation-btn--active')
@@ -13,3 +12,36 @@ $(function(){
         return false ;      
     });
 })  
+
+// Shop detail JS
+var reviewSelector = document.querySelectorAll('.shop-detail__content-review .reviews-title__item');
+var elementShowList = document.querySelectorAll('.hide-show-element');
+for (var i = 0; i < reviewSelector.length; i++) {
+    
+    reviewSelector[i].onclick = function () {
+        // for btn
+        console.log(this.getAttribute('data-type'))
+        for (var i = 0; i < reviewSelector.length; i++) {
+            reviewSelector[i].classList.remove('reviews-title__item--active');
+        }
+        this.classList.add('reviews-title__item--active');
+        
+        // for content
+        for (var i = 0; i < elementShowList.length; i++) {
+            elementShowList[i].classList.remove('shop-detail__content-review--active');
+        }
+        var elementShow = document.querySelector(this.getAttribute('data-type'));
+        elementShow.classList.add('shop-detail__content-review--active');
+    }
+}
+
+// Shop detail: rating
+var ratingElement = document.querySelectorAll('.send-comment__input-rating');
+for (var i = 0; i < ratingElement.length; i++) {
+    ratingElement[i].onclick = function () {
+        for (var i = 0; i < ratingElement.length; i++) {
+            ratingElement[i].classList.remove('send-comment__input-rating--active');
+        }
+        this.classList.add('send-comment__input-rating--active');
+    }
+}
